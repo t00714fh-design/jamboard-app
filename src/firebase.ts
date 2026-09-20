9528c7de835a44d80117fef7858d06c469175835
t00714fh-design <t00714fh@g.kofu-ymn.ed.jp>
2026-09-20 09:01:24 +0900

    fix: complete firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdCcvwgKiPi6a29304blX...", // ★元のapiKeyを入れてください
+  apiKey: "AIzaSyAdCcvwgKiPi6a293O4blxEkL91TMueU2A", // ★元のapiKeyを入れてください
   authDomain: "jamboard-app-48edd.firebaseapp.com",
   databaseURL: "https://jamboard-app-48edd-default-rtdb.firebaseio.com",
   projectId: "jamboard-app-48edd",
   storageBucket: "jamboard-app-48edd.appspot.com",
   messagingSenderId: "953568207405",
-  appId: "1:953568207405:web:b91cccee0c...", // ★元のappIdを入れてください
+  appId: "1:953568207405:web:b91cccee0dc82e3ea64ea4", // ★元のappIdを入れてください
   measurementId: "G-L39MX9LN3Q"
 };
 
